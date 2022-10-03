1f310ab8363d1b7b39a1aa9568b3cbe3bfa445d7
Raul <raul.laboriel@gmail>
2022-10-03 11:52:46 -0600

    a

--- src/routes/Database.js
+++ src/routes/Database.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     database: process.env.DB_NAME || 'linkshortener',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '@5BumbSe]Uf-8U!q'
+    password: process.env.DB_PASS || '@5BumbSe]Uf-8U!q',
+    ssl: true
 });
 
 module.exports = pool;
